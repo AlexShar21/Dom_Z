ea553544673ead8ec657142b0acd718205da5138
Alex <aisharyshev@gmail.com>
2022-09-06 14:22:57 +0300

    Max

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("1е число, ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("2е число, ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+if (number1 > number2)
+    {
+    Console.WriteLine("number1");
+    }
+if (number1 < number2)
+{
+    Console.WriteLine("number2");
+}
